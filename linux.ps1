0ff152b3549366b35f3514a8b6ef8f8e3b9a6b71
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2024-11-03 08:28:33 -0500

    sync

--- linux.ps1
+++
@@ -1,8 +0,0 @@
-docker run --rm -v ${PWD}:/src python:3.8-slim /bin/bash -c "
-    apt update && \
-    apt install -y binutils tcl8.6 tk8.6 && \
-    pip install pyinstaller yt-dlp pillow customtkinter && \
-    cd /src && \
-    pyinstaller /src/YouTubeDownloaderLinux.spec && \
-    tar -czvf dist/YouTube_Downloader_Linux.tar.gz -C dist/linux YouTube_Downloader_Linux
-"
